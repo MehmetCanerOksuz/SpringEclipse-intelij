7ed2a51a6a219bca59bc7143194a3cb4b7dd487f
MehmetCanerOksuz <131135591+MehmetCanerOksuz@users.noreply.github.com>
2023-05-22 13:45:44 +0300

    Abstract Soyut

---
+++ _018_MirasOrnek2/src/main/java/org/example/AppMain.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class AppMain {
+    public static void main(String[] args) {
+
+        Book book = new Book();
+        book.addAuthor("Caner Öksüz");
+        System.out.println(book.getAuthors()[0]);
+
+    }
+}
