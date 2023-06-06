1e085f503a20586e257d997567ebdd468c41806b
MehmetCanerOksuz <131135591+MehmetCanerOksuz@users.noreply.github.com>
2023-06-06 15:33:55 +0300

    Stream

---
+++ _049_StreamApiOrnekler/src/main/java/com/caneroksuz/_03_StreamdenListeye.java
@@ -0,0 +1,39 @@
+package com.caneroksuz;
+
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class _03_StreamdenListeye {
+
+    public static void main(String[] args) {
+
+        Stream<String> myStream = Stream.of("Ünal","Gani","Salih","Gizem","Abdullah","Ali","Ahmet","Aysu");
+        TreeSet<String> myTreeSet= myStream.collect(TreeSet::new, TreeSet::add,TreeSet::addAll);
+        System.out.println(myTreeSet);
+
+        System.out.println("---------------------------");
+
+        Stream<String>  myStream2 = Stream.of("Ünal","Gani","Salih","Gizem","Abdullah","Ali","Ahmet","Aysu");
+        TreeSet<String> myTreeSet2= myStream2.collect(Collectors.toCollection(TreeSet::new));
+
+        System.out.println("---------------------------");
+
+        Stream<String>  myStream3 = Stream.of("Ünal","Gani","Salih","Gizem","Abdullah","Ali","Ahmet","Aysu");
+        myStream3
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
+
+        System.out.println("---------------------------");
+
+        Stream<String>  myStream4 = Stream.of("Ünal","Gani","Salih","Gizem","Abdullah","Ali","Ahmet","Aysu");
+        TreeSet<String> myTreeSet4= myStream4
+                .filter(isim->isim.startsWith("A") || isim.contains("i") )
+                .collect(Collectors.toCollection(TreeSet::new));
+        System.out.println(myTreeSet4);
+
+    }
+}
