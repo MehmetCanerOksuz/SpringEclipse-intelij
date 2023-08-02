0aedefa963d426670572eb33fa749dd2ede01098
MehmetCanerOksuz <131135591+MehmetCanerOksuz@users.noreply.github.com>
2023-08-02 13:34:32 +0300

    02.08.2023

---
+++ -075_js/_003_degiskenler.js
@@ -0,0 +1,14 @@
+// var firstName;
+// var lastName;
+// var info, address;
+
+// var firstName ="Caner";
+// var lastName = 'Mehmet';
+// var info = 'Öğrenci', address = 'Berlinköy';
+
+let firstName ="Caner";
+let lastName = 'Mehmet';
+let info = 'Öğrenci', address = 'Berlinköy';
+
+document.getElementById('ogrenciBilgisi').innerHTML = 
+      'BİLGİ NOTU: '+ firstName + ' ' + lastName + '<br> Mesleği: ' + info;
